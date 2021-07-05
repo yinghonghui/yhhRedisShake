3d5d23aecfbb55bcf49c31583cd50854581b8165
yinghonghui <yinghonghui@tuhu.cn>
2021-07-05 13:54:31 +0800

    初始化脚本

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,8 @@
 wget https://golang.google.cn/dl/go1.16.5.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
+vim ~/.bashrc
 export PATH=$PATH:/usr/local/go/bin
+source ~/.bashrc
 set GOARCH=amd64
 set GOOS=linux
 export GOPROXY=https://goproxy.cn
