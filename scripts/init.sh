eaf70f13665cb44692c982395ce18f06b6bc81f6
yinghonghui <yinghonghui@tuhu.cn>
2021-07-31 16:41:36 +0800

    fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,6 @@
 wget https://golang.google.cn/dl/go1.16.5.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
-vim ~/.bashrc
-export PATH=$PATH:/usr/local/go/bin
+echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.bashrc
 source ~/.bashrc
 set GOARCH=amd64
 set GOOS=linux
@@ -9,3 +8,6 @@ export GOPROXY=https://goproxy.cn
 go build /root/yhhRedisShake/src/redis-shake/main
 mv /root/yhhRedisShake/src/main /root/yhhRedisShake/conf/
 ./main -type=sync -conf=/tmp/RedisShake/conf/redis-shake.conf
+
+./root/yhhRedisShake/conf/main -type=sync -conf=/tmp/RedisShake/conf/redis-shake.conf
+
